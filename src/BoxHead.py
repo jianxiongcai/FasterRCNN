2b3825b32210eb5238deccbb403a34ebe16073de
Jianxiong Cai <jicai@seas.upenn.edu>
2020-11-12 21:51:39 -0500

    [DONE, untested] 6.3 loss

--- src/BoxHead.py
+++ src/BoxHead.py
@@ -8,6 +8,7 @@ class BoxHead(torch.nn.Module):
     def __init__(self,Classes=3,P=7):
         self.C=Classes
         self.P=P
+
         # define network
         self.interm_layer = nn.Sequential(
             nn.Linear(256 * P * P, 1024),
@@ -17,13 +18,18 @@ class BoxHead(torch.nn.Module):
         )
 
         self.classifier = nn.Sequential(
-            nn.Linear(1024, self.C+1),
-            nn.Softmax(dim=1)
+            nn.Linear(1024, self.C+1)
+            # No softmax here. (Softmax performed in CrossEntropyLoss when computing loss)
+            # nn.Softmax(dim=1)
         )
 
         self.regressor = nn.Sequential(
             nn.Linear(1024, self.C * 4),
         )
+
+        # define loss
+        self.ce_loss = nn.CrossEntropyLoss(reduction='mean')
+        self.smooth_l1_loss = nn.SmoothL1Loss(reduction='mean')
 
 
     #  This function assigns to each proposal either a ground truth box or the background class (we assume background class is 0)
@@ -126,12 +132,90 @@ class BoxHead(torch.nn.Module):
     #      loss_class: scalar
     #      loss_regr: scalar
     def compute_loss(self,class_logits, box_preds, labels, regression_targets,l=1,effective_batch=150):
+        assert isinstance(labels, torch.LongTensor)
 
+        # do sampling (class balancing ~3:1)
+        class_logits_sampled, box_preds_sampled, \
+        labels_sampled, regression_targets_sampled = self.do_sampling(class_logits, box_preds,
+                                                                      labels, regression_targets,
+                                                                      effective_batch)
+
+        # cls loss
+        loss_class = self.ce_loss(class_logits_sampled, torch.squeeze(labels_sampled, dim=1))
+
+        # reg loss
+        # take all non-background bbox
+        box_list = [None, None, None]           # list: (num_pos_object, 4), only keep box correspond to gt object
+        gt_box_list = [None, None, None]
+        for gt_class in range(3):
+            # 1-d binary indicator: if labels_sampled[i] == gt_class
+            class_mask = (torch.squeeze(labels_sampled, dim=1) == (gt_class + 1))
+
+            # selection range for class i: [i*4, i*4+4)
+            box_selected = box_preds_sampled[class_mask, gt_class * 4: gt_class * 4 + 4]
+            gt_box_selected = regression_targets[class_mask]
+
+            box_list[gt_class] = box_selected
+            gt_box_list[gt_class] = gt_box_selected
+
+        # put all boxes together and compute loss
+        box_regr = torch.cat(box_list, dim=0)
+        gt_box_regr = torch.cat(gt_box_list, dim=0)
+        loss_regr = self.smooth_l1_loss(box_regr, gt_box_regr)
+
+        loss = loss_class * l * loss_regr
         return loss, loss_class, loss_regr
 
+    def do_sampling(self, class_logits, box_preds, labels, regression_targets, effective_batch):
+        """
 
+        All params refer to compute_loss()
+        :return:
+        """
+        # number of positive sample to keep after sampling
+        N_pos = int(0.75 * effective_batch)
 
-    # Forward the pooled feature vectors through the intermediate layer and the classifier, regressor of the box head
+        # do positive sampling
+        indicator_pos = torch.squeeze(labels != 0, dim=1)             # 1 dimensional
+        indicator_neg = torch.squeeze(labels == 0, dim=1)
+        indices_pos = torch.squeeze(torch.nonzero(indicator_pos), dim=1)
+        indices_neg = torch.squeeze(torch.nonzero(indicator_neg), dim=1)
+
+        if len(indices_pos) <= N_pos:       # take all available
+            print("[WARN, minor] Not enough positive samples. Expected: {}, Actual: {}".format(N_pos, len(indices_pos)))
+            N_pos = len(indices_pos)
+            indices_keep_pos = indices_pos
+        else:
+            indices_keep_pos = self.random_choice(indices_pos, N_pos)
+
+        # number of negative samples to keep
+        N_neg = effective_batch - N_pos
+        if len(indices_neg) < N_neg:       # take all available
+            assert len(indices_neg) >= N_neg
+            print("[WARN, major] Not enough negative samples. Expected: {}, Actual: {}".format(N_neg, len(indices_neg)))
+            N_neg = len(indices_neg)
+            indices_keep_neg = indices_neg
+        else:
+            indices_keep_neg = self.random_choice(indices_neg, N_neg)
+
+        # return sampled batch
+        return torch.cat([class_logits[indices_keep_pos], class_logits[indices_keep_neg]], dim=0), \
+               torch.cat([box_preds[indices_keep_pos], box_preds[indices_keep_neg]], dim=0), \
+               torch.cat([labels[indices_keep_pos], labels[indices_keep_neg]], dim=0), \
+               torch.cat([regression_targets[indices_keep_pos], regression_targets[indices_keep_neg]], dim=0),
+
+    @staticmethod
+    def random_choice(x, N_sample):
+        """
+
+        :param x: the input tensor to select from
+        :param N_sample: number of sample to keep
+        :return:
+        """
+        idx = torch.randperm(len(x))
+        return x[idx]
+
+            # Forward the pooled feature vectors through the intermediate layer and the classifier, regressor of the box head
     # Input:
     #        feature_vectors: (total_proposals, 256*P*P)
     # Outputs:
